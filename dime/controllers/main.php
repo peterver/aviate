805f32afba6a68a3a86d490bc8bd666c54cddc14
Visual Idiot <iam@visualidiot.com>
2012-12-04 09:29:09 +0000

    more rearranging

--- dime/controllers/main.php
+++ dime/controllers/main.php
@@ -1,29 +1,33 @@
 <?php defined('IN_APP') or die('Get out of here');
 
+/**
+ *   Dime: Main controller
+ *
+ *   This handles loading the storefront, but also checks for the existence of valid
+ *   config and system requirements. If it doesn't fit, it'll go to the installer to
+ *   try and sort that out.
+ */
+
 class Main_controller extends Controller {
+	//  Extend the main controller
+	//  We can also put anything we want to show in any controller method here
+	//  You know, so we don't repeat ourselves.
+	//  You know, so we don't repeat ourselves.
 	public function __construct() {
 		parent::__construct();
 		
-		$this->helper->load(array('lkdto', 'crush'));
-
-		//  Set template data
-		$this->template->set(array(
-			'language' => Config::get('language'),
-			'title' => 'Hiya, world.',
-			'heading' => 'Howdy, world!',
-			'route' => $this->routes,
-			
-			'query_count' => $this->database->queryCount()
-		));
+		//  Load the installer helper, which will handle redirection for us
+		$this->helper->load('installer');
 		
-		echo $this->template->render();
+		//  Set some defaults to use our custom theme paths instead of
+		//  Scaffold's defaults, make it a bit more discoverable
+		$this->template->set(array(
+			'view_base' => TEMPLATE_BASE . 'views/',
+			'partial_base' => TEMPLATE_BASE . 'partials/'
+		));
 	}
 	
 	public function index() {
-		echo $this->template->render('main');
-	}
-	
-	public function help() {
-		echo 'test';
+		echo $this->template->render('index');
 	}
 }
---
+++ theme/partials/scripts.php
@@ -0,0 +1,6 @@
+<script src="//code.jquery.com/jquery-latest.min.js"></script>
+<script src="/assets/js/site.js"></script>
+
+<?php if(Config::get('gosquared')): ?>
+	<?php echo 'analytics'; ?>
+<?php endif; ?>
