70edf8973acb415c968eebed197d7edc3cb491dd
Visual Idiot <iam@visualidiot.com>
2012-12-06 15:24:13 +0000

    more more more

--- theme/partials/header.php
+++ theme/partials/header.php
@@ -1,8 +1,17 @@
 <header id="top" class="header">
-	<div class="wrap">
-		<b class="left">
-			<a href="/" id="logo">My Dime Store</a>
-		</b>
+		<a href="/" id="logo">
+			<img src="/assets/img/dime-logo.png" alt="Dime logo" title="My Dime store">
+		</a>
+		
+		<ul class="nav">
+			<li><a href="/about">About Us</a>
+			<li><a href="/blog">Blog</a>
+			<li><a href="/contact">Contact Us</a>
+		</ul>
+		
+		<form id="search" class="right" action="/search">
+			<input class="col3" type="search" placeholder="Search My Dime Store">
+		</form>
 		
 		<p class="basket right">
 			<?php if(Basket::hasItems()): ?>
@@ -12,8 +21,6 @@
 			<?php endif; ?>
 		</p>
 		
-		<form id="search" class="right" action="/search">
-			<input class="col3" type="search" placeholder="Search My Dime Store">
-		</form>
+		
 	</div>
 </header>
--- theme/partials/scripts.php
+++ theme/partials/scripts.php
@@ -1,6 +1,3 @@
-<script src="//code.jquery.com/jquery-latest.min.js"></script>
-<script src="/assets/js/site.js"></script>
-
 <?php if(Config::get('gosquared')): ?>
 	<?php echo 'analytics'; ?>
 <?php endif; ?>
