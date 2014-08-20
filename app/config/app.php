5cd6fce07768da4895041aa7cae001892a756061
Visual Idiot <iam@visualidiot.com>
2014-08-20 15:00:48 +0100

    try this then shall we

--- app/config/app.php
+++ app/config/app.php
@@ -121,6 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		//'Cartalyst\Themes\ThemeServiceProvider'
 
 	),
 
@@ -188,7 +189,8 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-
+		'Theme'			  => 'Cartalyst\Themes\Facades\Theme',
+		'Asset'			  => 'Cartalyst\Themes\Facades\Asset',
 	),
 
 );
